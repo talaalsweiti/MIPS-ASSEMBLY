2a2c7a4341348c3718d907c90a3b6e22d2975967
talaalsweiti <talaalsweiti@gmail.com>
2023-11-23 21:25:06 +0200

    need changes

--- Calender.asm
+++ Calender.asm
@@ -10,7 +10,7 @@ newLine: .asciiz "\n"
 
 open_file_error: .asciiz"Error! opening the file"
 
-welcome_message: .asciiz "Welcome to our program!!\n"
+welcome_message: .asciiz "\n Welcome to our program!!\n"
 
 view_per_day: .asciiz "\n a - View calender per day\n"
 view_per_set_days: .asciiz "\n b - View calender per set of days\n"
@@ -21,7 +21,7 @@ view_meets_num: .asciiz "\n f - View meetings number\n"
 view_avg_lec: .asciiz "\n g - View the average lectures per day\n"
 view_ratio_lec_OH: .asciiz "\n h - View the ratio between total number of lectures and the total number of OH\n"
 quit: .asciiz "\n q - quit the program\n"
-quit_message: .asciiz "Thank you for using our program!!\n"
+quit_message: .asciiz "\n Thank you for using our program!!\n"
 
 select:.asciiz " \n Please choose a, b, c, d, e, f, g, h, or q: " 
 a: .asciiz "a\n"
@@ -33,15 +33,16 @@ f: .asciiz "f\n"
 g: .asciiz "g\n"
 h: .asciiz "h\n"
 q: .asciiz "q\n"
-
+yes: .asciiz "yes\n"
 
 choice: .space 10
 
-error: .asciiz "invalid input, try again\n"
-day_not_exist: .asciiz "Day not exist, try again\n"
+error: .asciiz "\n Invalid input, try again\n"
+day_not_exist: .asciiz "\n Day not exist, try again\n"
 select_day:.asciiz " \n Please choose a day: "  
 select_day_sentence_1:.asciiz " \n The slots for day "
- 
+another_day_q: .asciiz "\n Enter yes to enter another day, or any character to returb to menu: "
+another_day_answer : .space 10
 day: .space 10
 
 # to take the day input from the user
@@ -50,6 +51,19 @@ choiced_day_input: .space 10
 # the taken day without new line (used for comparision)
 choiced_day: .space 10
 day_slot :  .space 100
+line: .asciiz "\n ______________________________________________________________________ \n "
+
+
+# the taken day without new line (used for comparision)
+given_day: .space 10
+given_day_input: .space 10
+
+
+enter_slot: .asciiz "\n Please enter the slot sapereted by '-' :\n"
+given_slot: .space 20
+first_num: .space 10
+sec_num: .space 10
+invalid_slot: .asciiz "\n The enterd slot is not valid, please try again!\n"
 
 .text
 .globl main
@@ -66,6 +80,12 @@ START:
 	
 menu:
 	#printing the menue 
+	
+	la $a0, line		
+	li $a1, 256
+	li $v0, 4
+	syscall
+	
 	la $a0, view_per_day		
 	li $a1, 256
 	li $v0, 4
@@ -122,51 +142,52 @@ selection:
 	
 	# get the choice from the user
 	la $a0, choice
+	la $a3,choice
 	li $v0, 8
 	syscall
 	
-	# if the user chice a
+	# if the user choice to view per day
 	la $a1,a		
 	jal strcmp
 	beq $v0,$zero, get_day
 	
 	
-	# if the user chice b
-	#la $a1,b		
-	#jal strcmp
-	#beq $v0,$zero, 
+	# if the user choice to view per set of days
+	la $a1,b		
+	jal strcmp
+	beq $v0,$zero, get_set_of_days
 	
-	# if the user chice c
-	#la $a1,c		
-	#jal strcmp
-	#beq $v0,$zero, 
+	# if the user choice to view given slot in a given day
+	la $a1,c		
+	jal strcmp
+	beq $v0,$zero, get_given_slot_in_given_day
 	
-	# if the user chice d
+	# if the user choice d
 	#la $a1,d		
 	#jal strcmp
 	#beq $v0,$zero, 
 	
-	# if the user chice e
+	# if the user choice e
 	#la $a1,e		
 	#jal strcmp
 	#beq $v0,$zero, 
 	
-	# if the user chice f
+	# if the user choice f
 	#la $a1,f		
 	#jal strcmp
 	#beq $v0,$zero, 
 	
-	# if the user chice g
+	# if the user choice g
 	#la $a1,g		
 	#jal strcmp
 	#beq $v0,$zero, 
 	
-	# if the user chice h
+	# if the user choice h
 	#la $a1,h		
 	#jal strcmp
 	#beq $v0,$zero, 
 	
-	# if the user chice q
+	# if the user choice q
 	la $a1,q		
 	jal strcmp
 	beq $v0,$zero, exit
@@ -193,8 +214,10 @@ read_file:
 	bnez $v0, readFile
 	
 	# Todo : not working
-	li $v0, 4
+	
 	la $a0, open_file_error
+	li $a1, 256
+	li $v0, 4
 	syscall
 	
 	li $v0,4
@@ -220,6 +243,178 @@ read_file:
 		jr $ra 
 
 
+
+get_given_slot_in_given_day:
+	
+	la $a0, select_day		
+	li $a1, 256
+	li $v0, 4
+	syscall
+	
+	# get the choice from the user
+	la $a0, given_day_input
+	li $v0, 8
+	syscall		
+	
+	
+	la $t3, given_day_input
+	la $t2, choiced_day
+	jal remove_new_line
+	
+	la $a0, choiced_day	
+	li $v0, 4
+	syscall
+	
+	la $a0, given_day_input		
+	li $v0, 4
+	syscall
+	
+	#j menu
+	jal check_day
+	
+	beq $v0 ,$zero, get_slot_in_day
+
+	la $a0, day_not_exist		
+	li $v0, 4
+	syscall
+	#or j menu
+	j get_given_slot_in_given_day
+	
+	get_slot_in_day:
+		
+	la $a0, enter_slot		
+	li $a1, 256
+	li $v0, 4
+	syscall
+	
+	# get the choice from the user
+	la $a0, given_slot
+	li $v0, 8
+	syscall	
+	
+
+	la $t4,given_slot
+	la $t7,first_num 
+	la $t5,sec_num 	
+	
+	
+   	loop_1:
+        	lb $t6, ($t4)         
+        	addi $t4, $t4, 1
+        	#beqz $t6, end_loop_1   
+        	#beq $t6, '\n', end_line_1 
+        	beq $t6, '-', process_sec_num 
+
+        	sb $t6, 0($t7)         # Store the character in first_num buffer
+        	addi $t7, $t7, 1       # Move to the next position in day buffer
+        	j loop_1
+	
+	
+  	process_sec_num:
+        # Null-terminate the day buffer
+        sb $zero, 0($t7)	
+       store_sec_num:
+           	lb $t6, ($t4)
+           	addi $t4, $t4, 1
+           	beqz $t6, procces_nums    # Exit the loop if the null terminator is encountered
+           	beq $t6, '\n', procces_nums    # Exit the loop when newline is encountered
+           	beq $t6, '-', store_sec_num
+           	sb $t6, 0($t5) 
+           	addi $t5, $t5, 1
+            	j store_sec_num
+            
+         
+	procces_nums:
+	 	sb $zero, 0($t5)
+	 	
+	 	
+	 	move $a0, $t5
+	 	jal str_to_int
+	 	move $t5, $v0
+	 	
+	 	
+	 	move $a0, $t7
+	 	jal str_to_int
+	 	#move $t7, $v0
+	 	addi $t7, $v0, 12 
+	 	la $a0,  first_num		
+		li $v0, 4
+		syscall
+	 	# if num1 or num2 is equal to 6 or 7 then not valid
+		#beq $t7, 6, num_not_valid
+		#beq $t7, 7, num_not_valid
+			
+		#beq $t5, 7, num_not_valid
+		#beq $t5, 6, num_not_valid
+		
+		# if num1 is less than or equal 5, we need to add 12 
+		#ble $t7, 5 , add_12_num1
+		
+		#add_12_num1:
+		
+		#la $a0,  newLine	
+		#li $v0, 4
+		#syscall
+    		#addi $t7, $t7, 12  # Add 12 to the loaded value
+
+					
+		# if num2 is less than or equal 5, we need to add 12 
+		ble $t5, 5 , add_12_num2
+		add_12_num2:
+		  addi $t5, $t5, 12
+		  
+		  
+		j find_print_slot
+	
+		
+	num_not_valid:
+		la $a0, invalid_slot			
+		li $a1, 256
+		li $v0, 4
+		syscall
+	
+				
+	
+ 	find_print_slot:
+ 	#num1 is in t7 and num2 in t5
+ 		la $a0,  first_num		
+		li $v0, 4
+		syscall
+		
+		la $a0, sec_num		
+		li $v0, 4
+		syscall
+		
+		la $a1 , choiced_day
+ 		j menu
+ 
+ 
+
+get_set_of_days:
+	j get_day
+	
+	enter_another_day:
+	la $a0, another_day_q			
+	li $a1, 256
+	li $v0, 4
+	syscall
+	
+	
+	# get the choice from the user
+	la $a0, another_day_answer
+	li $v0, 8
+	syscall		
+
+	la $a1,yes		
+	jal strcmp
+	beq $v0,$zero, get_set_of_days
+	
+			
+	j menu
+	
+
+
+
 get_day:
 	la $a0, select_day			
 	li $a1, 256
@@ -233,7 +428,6 @@ get_day:
 	
 	la $t3, choiced_day_input
 	la $t2, choiced_day
-	
 	jal remove_new_line
 	
 	jal check_day
@@ -243,7 +437,6 @@ get_day:
 	la $a0, day_not_exist		
 	li $v0, 4
 	syscall
-	
 	#or j menu
 	j get_day
 		
@@ -265,12 +458,9 @@ get_day:
 
      process_slot:
         # Null-terminate the day buffer
-         sb $zero, 0($t7)
-	
-	
+        sb $zero, 0($t7)
 	la $a1, choiced_day
 	la $a0,day
-	
 	jal strcmp
 
 	
@@ -298,22 +488,31 @@ get_day:
  	slot_found: 
  	    	#print slot 
  	    	la $a0, select_day_sentence_1
+ 	    	li $a1, 256
 		li $v0, 4
 		syscall
 		la $a0,choiced_day
+		li $a1, 256
 		li $v0, 4
 		syscall
 		la $a0,newLine
 		li $v0, 4
 		syscall
  	    	la $a0, day_slot
+ 	    	li $a1, 256
 		li $v0, 4
 		syscall
 		la $a0,newLine
 		li $v0, 4
 		syscall
-		la $t5, day_slot
-		j end_loop_0
+		
+		#la $t5, day_slot
+		move $a0,$a3	
+		la $a1,a
+		jal strcmp
+		beq $v0,$zero, end_loop_0
+	
+		j enter_another_day
 		
 	end_line_0:
         # Continue to the next line
@@ -345,9 +544,7 @@ check_day:
 	
 	la $a1, choiced_day
 	la $a0,day
-	
 	jal strcmp
-
 	
 	# day is found
 	beq $v0,$zero, day_found
@@ -375,6 +572,7 @@ check_day:
 				
 	exit_find_day:
 		move  $ra, $s0	
+		la $a1, choiced_day
 		jr $ra
 	
 
@@ -428,12 +626,31 @@ remove_new_line:
 continue:
 	sb $zero, 0($t2)
 	jr $ra
+	
+str_to_int:
+    li $v0, 0            # Initialize result to 0
+    li $t0, 10           # Set divisor to 10 for decimal digits
 
+    loop_3:
+        lb $t1, 0($a0)    # Load the current character from the string
+        beqz $t1, done    # If the character is null (end of string), exit the loop
+
+        sub $t1, $t1, '0'  # Convert ASCII character to numerical value
+        mul $v0, $v0, $t0  # Multiply the current result by the divisor
+        add $v0, $v0, $t1  # Add the numerical value of the current digit
+        addi $a0, $a0, 1   # Move to the next character in the string
+        j loop_3             # Repeat the loop
+
+    done_3:
+        jr $ra             # Return with the result in $v0
 
 exit:
 	la $a0, quit_message
+	li $a1, 256
 	li $v0, 4
 	syscall
 
 	li $v0, 10
 	syscall	
+
+
